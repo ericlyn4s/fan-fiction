d0fe9dbda849ebf3466b8015d25b8da7628a9149
Eric Peterson <ericmpeterson04@gmail.com>
2024-02-20 18:15:52 -0700

    Added comments and post functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,25 +31,36 @@ router.get('/', async (req, res) => {
 });
 
 // GET one story
-router.get('/story/:id', async (req, res) => {
-  // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-  } else {
-    // If the user is logged in, allow them to view the Story
-    try {
-      const dbStoryData = await Story.findByPk(req.params.id);
+router.get('/story/:id', withAuth, async (req, res) => {
+  try {
+    const dbStoryData = await Story.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: ['body', 'createdAt'],
+          include: [User],
+        },
+        {
+          model: User,
+          attributes: ['username']
+        },
+      ],
+    })
 
-      const story = dbStoryData.get({ plain: true });
+    const story = dbStoryData.get({ plain: true });
 
-      res.render('story', { story, loggedIn: req.session.loggedIn });
+    res.render('story', { 
+      story,
+      loggedIn: req.session.loggedIn,
+    });
+
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
-  }
-});
+  });
 
+  // LOGIN route
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
---
+++ public/js/comment.js
@@ -0,0 +1,29 @@
+const commentFormHandler =  async (event) => {
+    event.preventDefault();
+
+  // New comment object creation. The user_id is pulled in the controller component, these variables can be pulled at this stage
+    const body = document.querySelector('#comment-body').value;
+    const post_route = window.location.pathname.lastIndexOf('/');
+    const post_id = parseInt(window.location.pathname.substring(post_route + 1));
+
+    // if body is given, post this new comment usinf a FETCH request to the backend
+    if (body) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ body, post_id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace(window.location.pathname);
+      } else {
+        alert('Failed to leave comment');
+      }
+    };
+  };
+  
+  document
+    .querySelector('#comment-form')
+    .addEventListener('submit', commentFormHandler);
+  
+  
